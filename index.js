d455442d21e3a1124dcd6e615ddfced37ad3b462
Augustin Ribreau <augustin.ribreau@gmail.com>
2022-02-01 18:21:59 +0100

    feat: debug and sexy debug message

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ require('dotenv').config();
 const SpotifyWebApi = require('spotify-web-api-node');
 const SerialPort = require('serialport');
 const port = new SerialPort(process.env.port, {baudRate: 9600});
+const Readline = require('@serialport/parser-readline');
+const { DateTime } = require("luxon");
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', (elem)=>{
+    if (elem === "requestData"){
+        sendDataToArduino();
+    }
+});
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.clientId,
@@ -17,19 +26,20 @@ spotifyApi.setAccessToken(token);
  * @param {String} total_duration_ms
  */
 function sendToArduino(get_current_music_name, progress_duration, total_duration_ms) {
-    // c = current time / p  = progression duration / t = total duration
+    // c = current music / p  = progression duration / t = total duration
     const data =
         `{"c":"${get_current_music_name}", "p":"${progress_duration}", "t":"${total_duration_ms}"}`;
-
+    if (process.env.DEBUG){
+        console.log(`--- Send "${get_current_music_name}" to Arduino at ${DateTime.now().toFormat("dd/LL/y HH:mm:ss:S")} ---`);
+    }
     port.write(data, (err) => {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
-        return console.log('message written');
     });
 }
 
-setInterval(() => {
+function sendDataToArduino() {
     spotifyApi.getMyCurrentPlayingTrack().then(res => {
         const progress_duration = res.body.progress_ms;
         const total_duration_ms = res.body.item.duration_ms;
@@ -38,4 +48,4 @@ setInterval(() => {
     }).catch(err => {
         console.log(err)
     })
-}, 10000);
+}
